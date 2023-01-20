7fee014e5b4227a1b0855845416aeb007c29298b
Asilbec <asilbekomonkulov2003@gmail.com>
2023-01-19 23:40:25 -0500

    removed useless modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Globe from "react-globe.gl";
-import { useRef, useState, useEffect, useMemo } from "react";
-import axios from "axios";
+import { useState, useEffect } from "react";
 import "../src/mapData/worldBorders.geojson";
-import * as d3 from "d3";
 import countriesz from "../src/mapData/worldBorders.geojson";
 
 function App() {
   const [countries, setCountries] = useState({ features: [] });
   const [hoverD, setHoverD] = useState();
-  const [clickedCountry, setClickedCountry] = useState(null);
   const [selectedCountries, setSelectedCountries] = useState([]);
+  var [selectionPool, setSelectionPool] = useState([]);
 
   useEffect(() => {
     // load data
     fetch(countriesz)
       .then((res) => res.json())
       .then(({ features }) => setCountries(features));
+
+    fetch(countriesz)
+      .then((res) => res.json())
+      .then(({ features }) => setSelectionPool(features));
   }, []);
 
   // function checkSelectedCountriesList(isoCode){
@@ -25,12 +26,36 @@ function App() {
   // }
 
   function addToSelectedCountries(e) {
-    console.log(e);
     if (selectedCountries.includes(e)) {
-      console.log("true");
+      console.log(e.properties.ADMIN, " already in list");
     } else {
       setSelectedCountries((selectedCountries) => [...selectedCountries, e]);
+      console.log(e.properties.ADMIN, " added to selected countries");
     }
+  }
+
+  function inAlreadyClicked(e) {
+    if (selectedCountries.includes(e)) {
+      return "blue";
+    }
+    return "black";
+  }
+
+  function randomlySelectedCountry(_without_replacement = true) {
+    if (selectionPool.length === 0) {
+      console.log("- - - - - - - - reseting selection pool - - - - - - - -");
+      selectionPool = [...countries];
+      selectRandomCountry();
+    } else {
+      selectRandomCountry();
+    }
+  }
+
+  function selectRandomCountry(_without_replacement = true) {
+    var randomCountry =
+      selectionPool[Math.floor(Math.random() * selectionPool.length)]; // select random country from the selection pool
+    selectionPool.splice(selectionPool.indexOf(randomCountry), 1); // remove selected country from selection pool
+    console.log("Randomly Selected Country: ", randomCountry.properties.ADMIN);
   }
 
   return (
@@ -39,30 +64,38 @@ function App() {
         <>
           {selectedCountries.length > 0 && (
             <>
-              {selectedCountries.map((item, index) => (
-                <p>{item.properties.ADMIN}</p>
+              {selectedCountries.slice(-3).map((item, index) => (
+                <h5 key={index}>{item.properties.ADMIN}</h5>
               ))}
             </>
           )}
         </>
       </div>
+      <button
+        className="RandomCountryGenerator"
+        onClick={randomlySelectedCountry}
+      >
+        {" "}
+        Generate Random Country{" "}
+      </button>
       <Globe
         id="globe"
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
         polygonsData={countries}
-        polygonAltitude={(d) => (d === hoverD ? 0.01 : 0.005)}
-        polygonCapColor={(d) => (d === hoverD ? "blue" : "black")}
+        polygonAltitude={(d) => (d === hoverD ? 0.05 : 0.005)}
+        // polygonCapColor={(d) => (d === hoverD ? "blue" : "black")}
+        polygonCapColor={(d) => inAlreadyClicked(d)}
         polygonSideColor={() => "white"}
         polygonStrokeColor={() => "white"}
-        polygonsTransitionDuration={100}
+        polygonsTransitionDuration={1}
         onPolygonHover={setHoverD}
         onPolygonClick={(e) => {
           addToSelectedCountries(e);
+          setSelectionPool(countries.slice(-10));
         }}
       />
     </div>
   );
 }
-
 export default App;
