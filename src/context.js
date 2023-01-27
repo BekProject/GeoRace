4db1bb1efbfa651d7057d62dbb23a96cf3d1d55c
Derek <darmfield2023@gmail.com>
2023-01-27 14:03:40 -0500

    added continent filtering references

--- src/context.js
+++ src/context.js
@@ -6,6 +6,8 @@ export const StateContext = ({ children }) => {
   const [menu, setMenu] = useState(true);
   const [stats, setStats] = useState(false);
   const globeRef = useRef();
+  const [continentFilter, setContinentFilter] = useState('all');
+
 
   const updateMenu = (e) => {
     setMenu(e);
@@ -19,15 +21,24 @@ export const StateContext = ({ children }) => {
     globeRef.current.controls().autoRotate = e;
   };
 
+  const updateSetContinent = (e) => {
+    setContinentFilter(e);
+    console.log('continenter filter updated');
+  }
+
+  
+
   return (
     <Context.Provider
       value={{
         menu,
         stats,
         globeRef,
+        continentFilter,
         updateMenu,
         updateStats,
         updateEarthSpin,
+        updateSetContinent,
       }}
     >
       {children}
