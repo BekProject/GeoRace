8b47f30f111bbdd9a5fe2eeebdf2d8041b1a51ca
Asilbec <asilbekomonkulov2003@gmail.com>
2023-02-02 05:24:34 -0500

    added changes to menu layout

--- src/components/menuComponents/mainMenu.js
+++ src/components/menuComponents/mainMenu.js
@@ -1,58 +1,114 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
 import "./menu.css";
-import { useStateContext } from "../../context";
 
 export default function MainMenu() {
-  const { updateScreen } = useStateContext();
   return (
     <div className="MainMenuContainer">
       <div className="MainMenuAccountMethodContainer">
-        <div style={{ width: "100%" }}>
-          <Typography textAlign="center" variant="h5" component="h5">
-            create an account
-          </Typography>
-          <Button
-            onClick={() => updateScreen(1)}
-            style={{ width: "100%" }}
-            variant="contained"
-          >
-            Create acount
-          </Button>
-        </div>
-        <div style={{ width: "100%" }}>
-          <Typography textAlign="center" variant="h5" component="h5">
-            existing account
-          </Typography>
-          <Button
-            onClick={() => updateScreen(1)}
-            style={{ width: "100%" }}
-            variant="contained"
-          >
-            Sign in
-          </Button>
-        </div>
-
-        {/* <ButtonGroup
-          style={{ width: "100%", marginTop: "10px" }}
-          variant="outlined"
-          aria-label="outlined button group"
-        >
-          <Button
-            onClick={() => console.log(0)}
-            style={{ width: "100%" }}
-            variant="contained"
-          >
-            Sign In
-          </Button>
-          <Button
-            onClick={() => console.log(1)}
-            style={{ width: "100%" }}
-            variant={"contained"}
-          >
-            Play as a Guest
-          </Button>
-        </ButtonGroup> */}
+        <section classNam="bg-gray-50 dark:bg-gray-900">
+          <div classNam="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div
+              style={{
+                backgroundColor: "#1F2937",
+                width: "100%",
+                borderRadius: "0.5rem",
+                borderColor: "#374151",
+                boxShadow:
+                  "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+              }}
+            >
+              <div style={{ padding: "1.5rem", marginTop: "1rem" }}>
+                <h1
+                  style={{
+                    color: "#ffffff",
+                    fontSize: "1.25rem",
+                    fontWeight: "700",
+                    letterSpacing: "-0.025em",
+                    lineHeight: "1.25",
+                  }}
+                >
+                  Sign in to your account
+                </h1>
+                <form classNam="space-y-4 md:space-y-6" action="#">
+                  <div>
+                    <label
+                      for="email"
+                      classNam="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      classNam="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="password"
+                      classNam="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      classNam="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                  </div>
+                  <div classNam="flex items-center justify-between">
+                    <div classNam="flex items-start">
+                      <div classNam="flex items-center h-5">
+                        <input
+                          id="remember"
+                          aria-describedby="remember"
+                          type="checkbox"
+                          classNam="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                          required=""
+                        />
+                      </div>
+                      <div classNam="ml-3 text-sm">
+                        <label
+                          for="remember"
+                          classNam="text-gray-500 dark:text-gray-300"
+                        >
+                          Remember me
+                        </label>
+                      </div>
+                    </div>
+                    <a
+                      href="/"
+                      classNam="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+                  <button
+                    type="submit"
+                    classNam="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  >
+                    Sign in
+                  </button>
+                  <p classNam="text-sm font-light text-gray-500 dark:text-gray-400">
+                    Don’t have an account yet?{" "}
+                    <a
+                      href={"/"}
+                      classNam="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    >
+                      Sign up
+                    </a>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
